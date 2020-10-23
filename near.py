3ee2bce35501bb75d33d82b69d2b1a4c9d9e5e67
ILister1 <ilister@qa.com>
2020-10-23 15:52:35 +0100

    first attempt at near challenge

---
+++ near.py
@@ -0,0 +1,11 @@
+def near(a,b):
+     if len(b) == len(a) - 1 :
+        print("True")
+     else:
+        print("False")
+
+
+near("reset", "rest")
+near("dragoon", "dragon")
+near("eave", "leave")
+near("sleet", "lets")
